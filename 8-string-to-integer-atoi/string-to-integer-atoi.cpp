ed68a84b7d2a6d67afe6be2b49740a1b903a7dcd
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-07-22 15:49:09 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (82.34%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,55 +1,39 @@
 class Solution {
+    bool isDigit(char c ){
+        return c>='0' && c<='9';
+    }
 public:
-bool isDigit(char c){
-    return c>='0' && c<='9';
+long long  solve(int i , long long num , int sign , string &s){
+    if(i==s.length() || !isDigit(s[i])) return num;
+    int digit = s[i]-'0';
+    if(num>INT_MAX/10){
+        return (sign==1) ? LLONG_MAX:LLONG_MIN;
+    }
+    else if(num==INT_MAX/10){
+        if(sign==1 && digit>7) return LLONG_MAX;
+        else if(sign==-1 && digit>8) return LLONG_MIN;
+    }
+   return solve(i+1 , num*10+digit , sign , s);
 }
-
     int myAtoi(string s) {
-        int i = 0;
-        long long num = 0;
-        int sign = 1;
-        // remobve whitespace
-        while(i<s.length() && s[i] == ' '){
-            i++;
+        int i = 0; 
+       
+         int sign = 1; 
+        while(i<s.length() && s[i] == ' '){ i++; } 
+         if(i<s.length()){ 
+            if(s[i]=='-'){ sign = -1;
+             i++; } 
+             else if(s[i]=='+') {
+                 i++;
+              } 
+              }
+           long long num=   solve(i, 0 , sign , s);
+          if (num == LLONG_MAX)
+    return INT_MAX;
 
-        }
-        // signedness
-        if(i<s.length()){
-            if(s[i]=='-'){
-                sign = -1;
-                i++;
-            }
-            else if(s[i]=='+') {
-                i++;
-            }
-        }
-        // conversion
-        while(i<s.length() && isDigit(s[i])){
-            //rounding
-            int digit = s[i]-'0';
-            // overflow check
-             if(num>INT_MAX/10){
-                if(sign==1) return INT_MAX;
-                else  return INT_MIN;
-            }
-           else if(num==INT_MAX/10){
-                if(sign==1){
-                    if(digit>7) return INT_MAX;
-                } else if(sign ==-1){
-                    if(digit>8) return INT_MIN;
-                }
-            }
-           
-            
-
-
-            
-            num = num*10+ digit;
-            i++;
-        }
-
-        
-return sign*num;
-        
+if (num == LLONG_MIN)
+    return INT_MIN;
+              
+        return sign*num;
     }
 };
