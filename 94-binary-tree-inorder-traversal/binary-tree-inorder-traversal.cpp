20e4441810edf723f021c4105a222786b588ea73
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-07 01:29:21 +0530

    Time: 2 ms (4.51%) | Memory: 10.7 MB (98.31%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // Morris inorder traversl
+
+        vector<int> ans;
+        if(root==NULL) return ans;
+        TreeNode*curr=root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+            // find ip
+            TreeNode*IP=curr->left;
+            while(IP->right!=NULL && IP->right!=curr){
+                IP=IP->right; //find ip
+            }
+            if(IP->right==NULL){
+                IP->right=curr; //create thread
+                curr=curr->left;
+            }
+        else{
+                IP->right=NULL; //delete thread
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+
+            }
+
+        }
+
+        return ans;
+        
+    }
+};
