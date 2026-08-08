07ee6230e404f808f5d950d07b8e7d896dbb75b3
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-08 23:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 34.3 MB (52.60%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+// find inorder sucessor
 TreeNode*getIS(TreeNode*root){
     while(root!=NULL && root->left!=NULL){
         root=root->left;
@@ -26,17 +27,20 @@ TreeNode*getIS(TreeNode*root){
             root->right=deleteNode(root->right,key);
         }
         else{
+            // no left child
             if(root->left==NULL){
                 TreeNode*temp=root->right;
                 delete root;
                 return temp;
             }
+            // no right child
            else  if(root->right==NULL){
                 TreeNode*temp=root->left;
                 delete root;
                 return temp;
             }
             else{
+                // two child
                 TreeNode*IS=getIS(root->right);
                 root->val=IS->val;
                 root->right=deleteNode(root->right,IS->val);
