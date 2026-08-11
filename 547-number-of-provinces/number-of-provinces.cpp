a6f13d8be3b96554453a050f400a959ce45cfdbf
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-11 16:18:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (50.36%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
+//bfs
 int province=0;
-void dfs(int src,vector<vector<int>>& isConnected , vector<bool> & vis){
-    if(vis[src]==true) return;
+void bfs(int src,vector<vector<int>>& isConnected , vector<bool> & vis){
+    queue<int> q;
+    q.push(src);
     vis[src]=true;
+    while(!q.empty()){
+        int city = q.front();
+        q.pop();
       for(int v = 0 ; v<isConnected.size(); v++){
-        if(isConnected[src][v]==1 && !vis[v]){
-            dfs(v,isConnected,vis);
+        if(isConnected[city][v]==1 && !vis[v]){
+            vis[v]=true;
+            q.push(v);
         }
+    }
     }
 }
     int findCircleNum(vector<vector<int>>& isConnected) {
@@ -16,7 +23,7 @@ void dfs(int src,vector<vector<int>>& isConnected , vector<bool> & vis){
         for(int v= 0 ; v<n ; v++){
             if(!vis[v]){
                 province++;
-             dfs(v,isConnected,vis);
+             bfs(v,isConnected,vis);
             }
         }
         return province;
