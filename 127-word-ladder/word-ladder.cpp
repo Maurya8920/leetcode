b5a8beebde434b81d330e6695098f3fa5390d622
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-13 14:28:31 +0530

    Time: 81 ms (38.58%) | Memory: 22.7 MB (19.76%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -17,7 +17,6 @@ public:
             for(int i = 0 ; i<word.length(); i++){
                 char original = word[i];
                 for(char c='a';c<='z' ; c++ ){
-                    if(c == original) continue;
                    word[i]=c;
                     if(words.count(word) && !vis.count(word)){
                         vis.insert(word);
