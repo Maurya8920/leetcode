94bd06bc53d2cd34d98605690cfa5cbd17f33fa3
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-15 11:28:16 +0530

    Time: 19 ms (52.39%) | Memory: 26.3 MB (25.58%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -5,32 +5,28 @@ public:
         int m= grid[0].size();
            if(grid[n-1][m-1]==1) return -1;
            if(grid[0][0]==1) return -1;
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-        queue<pair<int,int>> q;
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        queue<pair<int,pair<int,int>>> q;
         int delrow[]={-1,-1,-1,0,0,1,1,1};
         int delcol[]={-1,0,1,-1,1,-1,0,1};
-        q.push({0,0});
-        vis[0][0]=true;
-        int count=1;
+        dist[0][0]=1;
+        q.push({1,{0,0}});
         while(!q.empty()){
-            int size=q.size();
-            for(int i = 0 ; i<size; i++){
-                int row = q.front().first;
-                int col = q.front().second;
+            int dis = q.front().first;
+                int row = q.front().second.first;
+                int col = q.front().second.second;
                 q.pop();
-                if(row==n-1 && col==m-1) return count;
+                if(row == n-1 && col == m-1)
+    return dis;
                 for(int i  =0 ; i<8 ; i++){
                     int nrow= row+delrow[i];
                     int ncol = col+delcol[i];
-                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==0 && !vis[nrow][ncol]){
-                        vis[nrow][ncol]=true;
-                        q.push({nrow,ncol});
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==0 && dis+1<dist[nrow][ncol]){
+                        dist[nrow][ncol]=dis+1;
+                        q.push({dist[nrow][ncol],{nrow,ncol}});
                     }
                 }
             }
-            count++;
-           
-        }
      
         return -1;
 
