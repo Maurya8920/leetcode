2234c229e1e426f23a34436e7aad8621389cb6da
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-17 13:27:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (82.56%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,32 +1,74 @@
+class disjointset {
+    
+    public:
+    vector<int> rank,parent,size;
+    disjointset(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i =0 ; i<=n ; i++ ) {
+            parent[i]=i;
+            
+        }
+    }
+    int findpar(int node){
+        if(node==parent[node]) return node;
+        return parent[node]= findpar(parent[node]);
+    }
+    void unionbyrank(int u , int v){
+        int ulp_u=findpar(u);
+        int ulp_v=findpar(v);
+        if(ulp_u==ulp_v) return;
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_v]<rank[ulp_u]){
+            parent[ulp_v]=ulp_u;
+        }
+        else{
+            // same
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+
+        }
+    }
+      void unionbysize(int u , int v){
+        int ulp_u=findpar(u);
+        int ulp_v=findpar(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+              parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+
+
+};
 class Solution {
 public:
-//bfs
-
-void bfs(int src,vector<vector<int>>& isConnected , vector<bool> & vis){
-    queue<int> q;
-    q.push(src);
-    vis[src]=true;
-    while(!q.empty()){
-        int city = q.front();
-        q.pop();
-      for(int v = 0 ; v<isConnected.size(); v++){
-        if(isConnected[city][v]==1 && !vis[v]){
-            vis[v]=true;
-            q.push(v);
-        }
-    }
-    }
-}
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int province=0;
-        int n = isConnected.size();
-        vector<bool> vis(n,false);
-        for(int v= 0 ; v<n ; v++){
-            if(!vis[v]){
-                province++;
-             bfs(v,isConnected,vis);
+            int n = isConnected.size();
+        disjointset ds(n);
+
+    
+        for(int i = 0 ; i<n ; i++){
+            for(int j= 0; j<n ; j++){
+                if(isConnected[i][j]==1){
+                    ds.unionbyrank(i,j);
+
+                }
             }
         }
-        return province;
+        int count=0;
+        for(int i = 0 ; i<n ; i++){
+            if(ds.parent[i]==i) count++;
+        }
+        return count;
+
+        
     }
 };
