022eb84f3e1948332e2ccdf504f0744a69812f0f
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-22 00:10:14 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (95.30%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,44 +2,26 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n= intervals.size();
-        vector<vector<int>> ans;
-      bool vis =false;
-      bool merged=false;
-      int mergeindex=-1;
-        for(int i = 0 ; i<n ; i++){
-            if(intervals[i][1]>=newInterval[0] && intervals[i][0]<=newInterval[1]){
-             int mergedstart = min(intervals[i][0],newInterval[0]);
-              int mergedend = max(intervals[i][1],newInterval[1]);
-              
-              if(merged==false){
-              ans.push_back({mergedstart, mergedend});
-               merged=true;
-                mergeindex= ans.size()-1;
-              }
-             
-             ans[mergeindex][0]=mergedstart;
-              ans[mergeindex][1]=mergedend;
-              newInterval[0]=mergedstart;
-              newInterval[1]=mergedend;
-              vis=true;
-             
-             }
-              else if(intervals[i][0]>newInterval[1]){
-                if(vis==false){
-                  ans.push_back(newInterval);
-                  vis=true;
-                }
-                   ans.push_back(intervals[i]);
-              }
-             
-             else{
-              ans.push_back(intervals[i]);
-           
-               }
-                 }
-        if(vis==false){
-        ans.push_back(newInterval);
+        int i= 0;
+        vector<vector<int>> result;
+        // left part
+        while(i<n && intervals[i][1]<newInterval[0]){
+            result.push_back(intervals[i]);
+            i=i+1;
         }
-        return ans;
+        // overlap
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
+          i=  i+1;
+
+        }
+        result.push_back(newInterval);
+        // right part
+        while(i<n){
+            result.push_back(intervals[i]);
+            i=i+1;
+        }
+        return result;
     }
 };
