168006b52ba29e5cbaf2a548155624c25ae3b6e2
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-24 22:17:01 +0530

    Time: 6 ms (37.23%) | Memory: 15.1 MB (20.46%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+void perm(vector<int> & nums, int idx , vector<vector<int>> & result){
+    int n = nums.size();
+    if(idx==n){
+      result.push_back(nums);
+      return;  
+    }
+    unordered_set<int> s;
+    for(int i =idx ; i<n ; i++){
+        if(s.count(nums[i])) continue; // means visited
+        s.insert(nums[i]); // mark visited
+       
+        swap(nums[idx],nums[i]);
+        perm(nums,idx+1,result);
+        swap(nums[idx],nums[i]);
+        
+    }
+}
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        perm(nums,0,result);
+        return result;
+        
+        
+    }
+};
