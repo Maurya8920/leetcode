86c435d41abdfc71b987c11a6ae29cbc1afc3ee8
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-24 14:25:44 +0530

    Time: 3 ms (51.85%) | Memory: 14.1 MB (77.14%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-set<vector<int>> s;
 void combsum(vector<int> & candidates , int target,int i , vector<int> & ans , vector<vector<int>> & result){
     int n = candidates.size();
     if(i==n  || target<0){
         return;
     }
     if(target==0){
-        if(s.find(ans)==s.end()){
         result.push_back(ans);
-        s.insert(ans);
-        
-        }
          return;
     }
    ans.push_back(candidates[i]);
-// single exclusion
-     combsum(candidates,target-candidates[i],i+1,ans,result);
      // multiple exclusion
     combsum(candidates,target-candidates[i],i,ans,result);
     ans.pop_back();  // backtrack
