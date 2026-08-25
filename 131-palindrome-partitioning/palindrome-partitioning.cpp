20e6b954de3f1a6e565eb86bb5a1b03ce6cbf5f8
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-08-25 21:29:17 +0530

    Time: 31 ms (85.78%) | Memory: 53.6 MB (72.24%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-bool ispali(string &st){
-    int i = 0;
-    int j= st.length()-1;
+bool ispali(string &st,int i , int j){
     while(i<=j){
         if(st[i]!=st[j]){
             return false;
@@ -21,14 +19,8 @@ void pali(string &s , int idx,vector<string> & ans , vector<vector<string>> & re
         return;
     }
     for(int i = idx ; i<n ; i++){
-        string st = "";
-        for(int j=idx ; j<=i ; j++){
-            st.push_back(s[j]);
-
-        }
-        
-        if(ispali(st)){
-             ans.push_back(st);
+        if(ispali(s,idx,i)){
+             ans.push_back(s.substr(idx,i-idx+1));
              pali(s,i+1,ans,result);
         ans.pop_back();
  }
