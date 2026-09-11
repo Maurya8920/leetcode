b4bf02bde47e5cc4b08710cdfa7ad30b84c84ee1
Maurya8920 <161703087+Maurya8920@users.noreply.github.com>
2026-09-11 22:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (71.14%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+int dp[21][21];
+bool match(string &s1, string &s2, int n , int m){
+    if(n==0){
+        if(m%2!=0) return false;
+        for(int i =0 ; i<m && i+1<m ; i+=2){
+            if(s2[i+1]!='*') return false;
+
+            
+        }
+     return true;
+}
+   if(m==0 && n!=0) return false;
+   if(dp[n][m]!=-1) return dp[n][m];
+   if(s1[n-1]==s2[m-1]){
+    return dp[n][m]= match(s1,s2,n-1,m-1);
+   }
+   
+    if(s2[m-1]=='.') return dp[n][m]= match(s1,s2,n-1,m-1);
+    if(s2[m-1]=='*') {
+        return dp[n][m]= match(s1,s2,n,m-2)|| (s1[n-1]==s2[m-2] || s2[m-2]=='.') && match(s1,s2,n-1,m);
+    }
+
+   return false;
+ 
+}
+
+
+    bool isMatch(string s, string p) {
+        int n= s.length();
+        int m = p.length();
+        memset(dp,-1,sizeof(dp));
+        return match(s,p,n,m);
+        
+    }
+};
